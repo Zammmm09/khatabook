46cdf1dd640212c007c2233b3fb022fcb1307fed
Zaeem Ansari <zaeemansari6666@gmail.com>
2025-07-01 13:47:28 +0530

    🔧 Updated port config for Render

--- app.js
+++ app.js
@@ -253,4 +253,8 @@ app.get("/logout", (req, res) => {
 });
 
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
